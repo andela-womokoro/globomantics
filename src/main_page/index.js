adcd1e886e806f9f4b0d2be0b72dda9488c761f8
Wilson Omokoro <wilson.omokoro@andela.com>
2020-09-04 15:43:01 +0100

    modify header component to use props.

--- src/main_page/index.js
+++ src/main_page/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './main_page.css';
 import Header from './header';
 
 function App() {
   return (
     <div className="container">
-        <Header />
+        <Header subTitle="Providing houses all over the  world" />
     </div>
   );
 }
